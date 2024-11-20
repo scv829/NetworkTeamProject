0aaa5ab7444163e944afd95d0671268b9a1e2de3
LeeJaeHo-96 <towpam123@gmail.com>
2024-11-20 14:49:54 +0900

    LJH_mapMaking

---
+++ Assets/LeeJaeHo/Scripts/Boom_ljh.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Boom_ljh : MonoBehaviour
+{
+    
+
+
+
+    public void Boom()
+    {
+        //폭발 애니메이션
+        //플레이어 죽음 함수 호출
+    }
+}
