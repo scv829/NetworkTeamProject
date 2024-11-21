8b3ae9acb20219fe7731ebddc0ef6f0b3473478a
gojjang10 <gojjang2@gmail.com>
2024-11-21 13:04:34 +0900

    Feat : 1p 헤이호 멀리보내기 기능 구현
    1p 헤이호 멀리보내기 기능 구현
    
    Related to : #2

---
+++ Assets/GoHyeonSeok/Scripts/KHS_MechaMarathonGameManager.cs
@@ -0,0 +1,164 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KHS_MechaMarathonGameManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private KHS_PlayerController[] _playerController;  // TODO : 배열 초기화 해야함. 매치메이킹 이후에 이루어지는 작업진행
+    public KHS_PlayerController[] PlayerController { get { return _playerController; } set { _playerController = value; } }
+
+    [SerializeField] private KHS_HeyHoController[] _heyHoController;    // TODO : 배열 초기화 해야함. 매치메이킹 이후에 이루어지는 작업진행
+    public KHS_HeyHoController[] HeyHoController { get { return _heyHoController; } set { _heyHoController = value; } }
+
+    [SerializeField] private bool _isStarted;
+    public bool IsStarted { get { return _isStarted; } set { _isStarted = value; } }
+
+    [SerializeField] private bool _isFinished;
+    public bool IsFinished { get { return _isFinished; ; } set { _isFinished = value; } }
+
+    //[SerializeField] private Player[] _sortedPlayers;
+    //public Player[] SortedPlayer { get { return _sortedPlayers; } set { _sortedPlayers = value; } }
+
+    private float _gameTimer;
+    public int[] _totalCount;   // TODO : 배열 초기화 해야함. 매치메이킹 이후에 이루어지는 작업진행
+
+    // Start 함수
+    private void Start()
+    {
+        // 게임씬에 진입했을시에는 false
+        IsStarted = false;
+    }
+
+    // Update 함수
+    private void Update()
+    {
+        // 게임 진행중이 아니면 return
+        if (IsStarted == false)
+            return;
+
+        // 게임 시간 측정
+        _gameTimer += Time.deltaTime;
+        //Debug.Log($"현재 진행중인 시간 : {_gameTimer}");
+
+        if (_gameTimer >= 5f) // 5초가 지나가면
+        {
+            // 게임 종료
+            IsStarted = false;
+
+            // 헤이호 출발을 위한 true
+            IsFinished = true;
+
+            // 타이머는 0으로
+            //_gameTimer = 0;
+
+            Debug.Log("게임 종료!");
+
+            // 플레이어 컨트롤러 배열의 길이만큼 반복
+            for (int i = 1; i < _playerController.Length; i++)
+            {
+                // 현재 플레이어 컨트롤러가 배열에 할당되지 않았으면 break.
+                if(_playerController[i] == null)
+                    break;
+                // 플레이어들의 총합 입력수를 입력
+                _totalCount[i] = _playerController[i].TotalInputCount;
+                Debug.Log($"Player {i} 총 입력 횟수 : {_totalCount[i]}");
+            }
+
+        }
+
+    }
+
+    // 방에 참가했을때 호출되는 함수
+    public override void OnJoinedRoom()
+    {
+        // 준비시간 조금 가지는 코루틴 실행
+        StartCoroutine(StartDelayRoutine());
+    }
+
+    // 네트워크에 진입 후 준비에 필요한 시간 살짝 주는 함수
+    IEnumerator StartDelayRoutine()
+    {
+        yield return new WaitForSeconds(1f);    // 네트워크 준비에 필요한 시간 살짝 주기
+        TestGameStart();
+    }
+
+    // 테스트 게임씬 시작 함수
+    public void TestGameStart()
+    {
+        Debug.Log("게임 시작");
+        // TODO : 테스트용 게임 시작 부분
+        PlayerSpawn();
+
+        //if (PhotonNetwork.IsMasterClient == false)
+        //    return;
+
+        //헤이호 생성
+        HeyHoSpawn();
+
+        // 플레이어, 헤이호 생성된 후 게임시작 카운트 다운 들어가기
+        StartCoroutine(DelayGameStart());
+    }
+
+    // 플레이어, 헤이호의 위치를 위한 함수
+    private Vector3 SetPosition()
+    {
+        // 현재 자신의 ActorNumber 대로 위치 설정
+        switch (PhotonNetwork.LocalPlayer.ActorNumber)
+        {
+            case 1:
+                return new Vector3(-7f, 1f, -8f);
+            case 2:
+                return new Vector3(-3f, 1f, -8f);
+            case 3:
+                return new Vector3(3f, 1f, -8f);  
+            case 4:
+                return new Vector3(7f, 1f, -8f);
+        }
+        return Vector3.zero;
+    }
+
+    // 플레이어 스폰 함수
+    private void PlayerSpawn()
+    {
+        Vector3 spawnPosition = SetPosition();
+
+        //Color color = Random.ColorHSV();
+        //object[] data = { color.r, color.g, color.b };
+
+        PhotonNetwork.Instantiate("KHS/KHS_Player", spawnPosition, Quaternion.identity/*, data : data*/);
+    }
+
+    // 헤이호 스폰 함수
+    private void HeyHoSpawn()
+    {
+        Vector3 spawnPosition = SetPosition();
+        spawnPosition.z += 2f;
+
+        //Color color = Random.ColorHSV();
+        //object[] data = { color.r, color.g, color.b };
+
+        PhotonNetwork.Instantiate("KHS/KHS_Hey-Ho", spawnPosition, Quaternion.identity/*, data : data*/);
+
+    }
+
+    WaitForSeconds _delay = new WaitForSeconds(1f);
+
+    // 카운트다운 후 게임 시작 함수
+    private IEnumerator DelayGameStart()
+    {
+
+        Debug.Log("3!");
+        yield return _delay;
+
+        Debug.Log("2!");
+        yield return _delay;
+
+        Debug.Log("1!");
+        yield return _delay;
+
+        Debug.Log("Start!!!");
+        IsStarted = true; // 게임 시작 상태로 변경
+    }
+}
--- Assets/GoHyeonSeok/Scripts/KHS_PlayerController.cs
+++ Assets/GoHyeonSeok/Scripts/KHS_PlayerController.cs
@@ -1,30 +1,46 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KHS_PlayerController : MonoBehaviour
+public class KHS_PlayerController : MonoBehaviourPun
 {
     [SerializeField] private int _totalInputCount;
     public int TotalInputCount { get { return _totalInputCount; } set { _totalInputCount = value; } }
 
-    [SerializeField] private KHS_TestGameScean _testScean;
+    [SerializeField] private KHS_MechaMarathonGameManager _mechaMarathonGameManager;
     [SerializeField] private KHS_HeyHoController _heyHoController;
+
 
     private void Awake()
     {
-        _testScean = FindAnyObjectByType<KHS_TestGameScean>();
-        _heyHoController = FindAnyObjectByType<KHS_HeyHoController>();
+        // (임시) 해당 오브젝트가 생성되었을때 게임매니저를 찾는 경우
+        _mechaMarathonGameManager = FindAnyObjectByType<KHS_MechaMarathonGameManager>();
+
+        // (임시) 해당 오브젝트가 생성되었을때 헤이호를 찾는 경우
+        //_heyHoController = FindAnyObjectByType<KHS_HeyHoController>();
+    }
+
+    private void Start()
+    {
+        // 현재 접속된 자신의 ActorNumber대로 이 스크립트를 참조시켜준다.
+        _mechaMarathonGameManager.PlayerController[PhotonNetwork.LocalPlayer.ActorNumber] = this;
+        Debug.Log($"{PhotonNetwork.LocalPlayer.ActorNumber}번째 플레이어");
     }
 
     private void Update()
     {
         // 시작하지 않은 상태면 return
-        if (_testScean.IsStarted == false)
+        if (_mechaMarathonGameManager.IsStarted == false)
             return;
 
-        if(Input.GetKeyDown(KeyCode.J))
+        if(photonView.IsMine)
         {
-            TotalInputCount++;
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                TotalInputCount++;
+            }
         }
+        else return;
     }
 }
