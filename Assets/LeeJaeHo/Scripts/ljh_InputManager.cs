d3b237b7d519d8a1fe0dd1b87da6d356f7605983
LeeJaeHo-96 <towpam123@gmail.com>
2024-11-22 23:45:46 +0900

    Fix : 버튼 선택 수정(완료)

--- Assets/LeeJaeHo/Scripts/ljh_InputManager.cs
+++ Assets/LeeJaeHo/Scripts/ljh_InputManager.cs
@@ -5,51 +5,70 @@ using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+
 enum State
-{ 
+{
     idle,
     move,
-    choice
+    choice,
+    end
 };
-
 
 public class ljh_InputManager : MonoBehaviourPun
 {
-    [SerializeField] PhotonView photonView;
-
+    int curUserNum;
+    
     [SerializeField] ljh_UIManager uiManager;
 
     [SerializeField] GameObject player;
     [SerializeField] GameObject button;
     [SerializeField] GameObject spotlight;
 
-
+    [Header("유저가 서는 자리 좌표")]
     [SerializeField] Vector3 Pos1;
     [SerializeField] Vector3 Pos2;
     [SerializeField] Vector3 Pos3;
     [SerializeField] Vector3 Pos4;
     [SerializeField] Vector3 Pos5;
 
+    [Header("유저가 서는 자리 오브젝트")]
     [SerializeField] public GameObject PosObj1;
     [SerializeField] public GameObject PosObj2;
     [SerializeField] public GameObject PosObj3;
     [SerializeField] public GameObject PosObj4;
     [SerializeField] public GameObject PosObj5;
 
+    [Header("버튼 오브젝트")]
     [SerializeField] public GameObject buttonObj1;
     [SerializeField] public GameObject buttonObj2;
     [SerializeField] public GameObject buttonObj3;
     [SerializeField] public GameObject buttonObj4;
     [SerializeField] public GameObject buttonObj5;
 
+    [Header("n명 남은 상황의 버튼 부모 오브젝트")]
+    [SerializeField] GameObject buttonParent4;
+    [SerializeField] GameObject buttonParent3;
+    [SerializeField] GameObject buttonParent2;
+
+    [Header("n명 남은 상황의 자리 부모 오브젝트")]
+    [SerializeField] GameObject posParent4;
+    [SerializeField] GameObject posParent3;
+    [SerializeField] GameObject posParent2;
+
     Vector3[] buttonPos;
 
-    State curState;
+    [SerializeField] State curState;
     public Vector3 _curPos;
+
+    [SerializeField] GameObject boom;
+    [SerializeField] GameObject door;
+    [SerializeField] GameObject sunLight;
 
     int defaultIndex;
     int minus;
     [SerializeField] int index;
+
+    public Coroutine _boomCoroutine;
 
     private void OnEnable()
     {
@@ -57,23 +76,59 @@ public class ljh_InputManager : MonoBehaviourPun
     }
     private void Start()
     {
-        defaultIndex = 2;
+        defaultIndex = 2;// 4인일땐 2 3인일땐 2로 
         minus = 0;
     }
 
     private void Update()
     {
-        
-         //Comment : 테스트용도
+
+        //Comment : 테스트용도
         if (Input.GetKeyDown(KeyCode.R))
         {
             curState = State.choice;
         }
         if (Input.GetKeyDown(KeyCode.T))
+        {
             curState = State.idle;
+        }
+
+        UserNumCalculate(curUserNum);
+
+        Playing();
 
 
-        switch(curState)
+
+    }
+
+
+    public void UserNumCalculate(int curUserNum)
+    {
+        switch (curUserNum)
+        {
+            case 4:
+
+                defaultIndex = 2;
+                //4인플
+                break;
+
+            case 3:
+
+                defaultIndex = 2;
+                //3인플
+                break;
+
+            case 2:
+
+                defaultIndex = 1;
+                //2인플
+                break;
+        }
+    }
+
+    public void Playing()
+    {
+        switch (curState)
         {
             case State.idle:
                 uiManager.ShowUiIdle();
@@ -83,16 +138,18 @@ public class ljh_InputManager : MonoBehaviourPun
             case State.move:
                 uiManager.ShowUiMove();
                 break;
-            
+
             case State.choice:
                 uiManager.ShowUiChoice();
                 _curPos = ChoiceAnswer();
                 SelectButton(_curPos);
                 break;
 
-        }
-        
+            case State.end:
+                GameEnd();
+                break;
 
+        }
     }
 
     public void FindPlayer()
@@ -104,43 +161,97 @@ public class ljh_InputManager : MonoBehaviourPun
 
     public Vector3 ChoiceAnswer()
     {
+        curUserNum = 2;
+
+        ljh_Button[] buttonObj = MakeButtonArray(curUserNum);
+        ljh_Pos[] pos = MakePosArray(curUserNum);
+
         index = defaultIndex - minus;
 
         if (Input.GetKeyDown(KeyCode.A) && index >= 1)
         {
             minus++;
         }
-        else if (Input.GetKeyDown(KeyCode.D) && index <= 3)
+        else if (Input.GetKeyDown(KeyCode.D) && index < buttonObj.Length-1)
         {
             minus--;
         }
 
-        Vector3[] Pos= 
-        { 
-            PosObj1.transform.position,
-            PosObj2.transform.position,
-            PosObj3.transform.position,
-            PosObj4.transform.position,
-            PosObj5.transform.position
-        } ;
-        GameObject[] buttonObj = { buttonObj1, buttonObj2, buttonObj3, buttonObj4, buttonObj5 };
 
         spotlight.transform.LookAt(buttonObj[index].transform.position);
-        
-        Vector3 curPos;
-        return curPos = Pos[index];
+
+        return pos[index].transform.position;
 
     }
 
-    public void SelectButton(Vector3 pos)
+    public void SelectButton(Vector3 curPos)
     {
-        Vector3[] buttonPos = { Pos1, Pos2, Pos3, Pos4, Pos5 };
-        GameObject[] buttonObj = { buttonObj1, buttonObj2, buttonObj3, buttonObj4, buttonObj5 };
+        ljh_Button[] buttonObj = MakeButtonArray(curUserNum);
+        ljh_Pos[] pos = MakePosArray(curUserNum);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (pos == buttonPos[index])
-                button.GetComponent<ljh_Button>().PushedButton(buttonObj[index]);
+           if (curPos == pos[index].transform.position)
+              // button.GetComponent<ljh_Button>().PushedButton(buttonObj[index]);
+            buttonObj[index].GetComponent<ljh_Button>().PushedButton(buttonObj[index]);
+            if (_boomCoroutine == null)
+                _boomCoroutine = StartCoroutine(BoomCoroutine(buttonObj[index]));
         }
+    }
+
+    IEnumerator BoomCoroutine(ljh_Button _button)
+    {
+        boom.GetComponent<ljh_Boom>().Vibe();
+        yield return new WaitForSeconds(Random.Range(1.5f,2.5f));
+        _button.SelectedButtonAction();
+
+    }
+
+    public ljh_Button[] MakeButtonArray(int curUser)
+    {
+        if (curUser == 4)
+        {
+            ljh_Button[] buttonParents = buttonParent4.GetComponent<ljh_ButtonParent>().buttonArray;
+            return buttonParents;
+        }
+        else if (curUser == 3)
+        {
+            ljh_Button[] buttonParents = buttonParent3.GetComponent<ljh_ButtonParent>().buttonArray;
+            return buttonParents;
+        }
+        else if (curUser == 2)
+        {
+            ljh_Button[] buttonParents = buttonParent2.GetComponent<ljh_ButtonParent>().buttonArray;
+            return buttonParents;
+        }
+        else
+            return null;
+    }
+
+    public ljh_Pos[] MakePosArray(int curUser)
+    {
+        if (curUser == 4)
+        {
+            ljh_Pos[] PosParents = posParent4.GetComponent<ljh_PosParent>().posArray;
+            return PosParents;
+        }
+        else if (curUser == 3)
+        {
+            ljh_Pos[] PosParents = posParent3.GetComponent<ljh_PosParent>().posArray;
+            return PosParents;
+        }
+        else if (curUser == 2)
+        {
+            ljh_Pos[] PosParents = posParent2.GetComponent<ljh_PosParent>().posArray;
+            return PosParents;
+        }
+        else
+            return null;
+    }
+
+    void GameEnd()
+    {
+        door.transform.rotation = Quaternion.Euler(0,90,0);
+        sunLight.transform.rotation = Quaternion.Euler(130, 48, 0);
     }
 }
--- Assets/LeeJaeHo/Scripts/ljh_Player.cs
+++ Assets/LeeJaeHo/Scripts/ljh_Player.cs
@@ -9,7 +9,7 @@ using UnityEngine.UIElements;
 public class ljh_Player : MonoBehaviourPun
 {
     //[SerializeField] ljh_InputManager inputManagerScript;
-    [SerializeField] ljh_inputTTTTT inputManagerScript;
+    [SerializeField] ljh_InputManager inputManagerScript;
     [SerializeField] GameObject inputManager;
 
     Vector3 buttonPos1;
@@ -30,7 +30,7 @@ public class ljh_Player : MonoBehaviourPun
         if(!photonView.IsMine)
             return;
 
-        Vector3 vec = inputManager.GetComponent<ljh_inputTTTTT>()._curPos;
+        Vector3 vec = inputManager.GetComponent<ljh_InputManager>()._curPos;
 
         if (vec != Vector3.zero)
         {
@@ -52,29 +52,7 @@ public class ljh_Player : MonoBehaviourPun
 
     public void MovePlayer(Vector3 vector)
     {
-       inputManagerScript = GameObject.FindWithTag("GameController").GetComponent<ljh_inputTTTTT>();
-       inputManager = GameObject.FindWithTag("GameController");
-     
-       buttonObj1 = inputManager.GetComponent<ljh_inputTTTTT>().buttonObj1;
-       buttonObj2 = inputManager.GetComponent<ljh_inputTTTTT>().buttonObj2;
-       buttonObj3 = inputManager.GetComponent<ljh_inputTTTTT>().buttonObj3;
-       buttonObj4 = inputManager.GetComponent<ljh_inputTTTTT>().buttonObj4;
-       buttonObj5 = inputManager.GetComponent<ljh_inputTTTTT>().buttonObj5;
-     
-       Vector3[] buttonPos = { buttonPos1, buttonPos2, buttonPos3, buttonPos4, buttonPos5 };
-       GameObject[] buttonObj = { buttonObj1, buttonObj2, buttonObj3, buttonObj4, buttonObj5 };
-     
-       
-        Vector3 playerVec = vector;
-
         transform.position = vector;
-        //if(vector == buttonPos[0])
-         //   gameObject.transform.position = ;
-
-       // gameObject.transform.position = ;
-
-        // 캐릭터의 위치를 vector로 이동시킴
-
     }
 
 
