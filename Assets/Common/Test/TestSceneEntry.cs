f4ddf8d16f3fe32f99bd2928dfd8da38e64b2be0
scv829 <scv829@naver.com>
2024-11-25 12:35:13 +0900

    Refactor: 주석 추가 및 로직 수정
    - 닉네임 설정 부분 수정
    - 주석 추가
    
    Related to: #29

--- Assets/Common/Test/TestSceneEntry.cs
+++ Assets/Common/Test/TestSceneEntry.cs
@@ -5,15 +5,18 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// 게임 씬 선택 프리팹의 스크립트
+/// </summary>
 public class TestSceneEntry : MonoBehaviour
 {
-    [SerializeField] TMP_Text sceneName;
-    [SerializeField] Button startButton;
+    [SerializeField] TMP_Text sceneName;        // 게임 씬의 이름
+    [SerializeField] Button startButton;        // 시작 버튼
 
     public void SetInfo(string sceneName)
     {
-        this.sceneName.text = sceneName;
-        startButton.onClick.AddListener(StartGame);
+        this.sceneName.text = sceneName;                // 생성시 설정한 이름으로 설정
+        startButton.onClick.AddListener(StartGame);     // 버튼에게 게임 시작 이벤트를 달아준다.
     }
 
     public void StartGame()
