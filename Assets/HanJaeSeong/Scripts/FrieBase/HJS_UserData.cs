7a7e5577320670a444aa06af7a94c6eaabb41fe0
scv829 <scv829@naver.com>
2024-11-27 17:27:38 +0900

    Refactor: 유저 엔티티 수정
    - 유저 엔티티 수정
    - 설명 속성 추가
    
    Related to: #47

--- Assets/HanJaeSeong/Scripts/FrieBase/HJS_UserData.cs
+++ Assets/HanJaeSeong/Scripts/FrieBase/HJS_UserData.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 플레이어 엔티티
+/// </summary>
 public class HJS_UserData
 {
-    public string name;
-    public string email;
-    public Record record = new Record();
+    public string name;         // 유저의 닉네임
+    public string email;        // 유저의 이메일(Id)
+    public string description;  // 유저의 소개
+    public Record record = new Record();    // 유저의 게임 기록
 }
 
 [System.Serializable]
 public class Record
 {
-    public int total;
-    public int win;
-    public int lose;
+    public int total;   // 총 판수
+    public int win;     // 승리한 판수
+    public int lose;    // 패배한 판수
 
-    public void Reset()
+    public void Reset() // 초기화 시키는 함수 -> 맨 처음 저장할 때
     {
         total = 0;
         win = 0;
