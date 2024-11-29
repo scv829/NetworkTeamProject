aca40f849e700915119472077e6cd0e56973c27f
gojjang10 <gojjang2@gmail.com>
2024-11-29 11:50:27 +0900

    Feat : 헤이호 멀리날리기 쿨타임 UI 추가
    헤이호 멀리날리기 쿨타임 UI 추가

--- Assets/GoHyeonSeok/Scripts/Mecha Marathon/KHS_UIManager.cs
+++ Assets/GoHyeonSeok/Scripts/Mecha Marathon/KHS_UIManager.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KHS_UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _text;
     [SerializeField] private WaitForSeconds _delay;
+    [SerializeField] private GameObject _coolTimeObj;
+    [SerializeField] private Image _coolTimeImage;
 
     private void Start()
     {
         _delay = new WaitForSeconds(1);
+        _coolTimeObj.SetActive(false);
     }
 
     public void CountDownGame()
@@ -30,9 +34,29 @@ public class KHS_UIManager : MonoBehaviour
         yield return _delay;
 
         _text.text = "Game Start!";
+        _coolTimeObj.SetActive(true);
+        StartCoroutine(CoolTimeCoroutine());
         yield return _delay;
        
         _text.text = "";
+
+    }
+
+    private IEnumerator CoolTimeCoroutine()
+    {
+        _coolTimeImage.fillAmount = 0;
+        float duration = 5f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            _coolTimeImage.fillAmount = elapsedTime / duration;
+            yield return null;
+        }
+
+        _coolTimeImage.fillAmount = 1;
+        _coolTimeObj.SetActive(false);
     }
 
     public void OnWinner(string winner)
