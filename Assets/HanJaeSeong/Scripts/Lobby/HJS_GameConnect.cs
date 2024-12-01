5ceeb3254806f7d7aff14be69a61ee70b396ddba
scv829 <scv829@naver.com>
2024-12-01 22:18:59 +0900

    Feat: 씬 연결 추가
    - 씬 연결하는 스트립트 추가
    - Fade in & out 연출 추가

--- Assets/HanJaeSeong/Scripts/Lobby/HJS_GameConnect.cs
+++ Assets/HanJaeSeong/Scripts/Lobby/HJS_GameConnect.cs
@@ -2,13 +2,17 @@ using Firebase.Auth;
 using Firebase.Database;
 using Firebase.Extensions;
 using Fusion;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HJS_GameConnect : SimulationBehaviour, IPlayerJoined
 {
     [SerializeField] Transform spawnPoint;
     [SerializeField] GameObject PlayerPrefab;
+    [SerializeField] HJS_FadeController fadeController;
 
     // 플레이어가 들어왔을 때
     public void PlayerJoined(PlayerRef player)
@@ -16,7 +20,15 @@ public class HJS_GameConnect : SimulationBehaviour, IPlayerJoined
         if (player == Runner.LocalPlayer)
         {
             Runner.Spawn(PlayerPrefab, HJS_PlayerPosition.Instance.PlayerPos, Quaternion.identity);
+            Runner.StartCoroutine(FadeRoutine());
         }
+    }
+
+    private IEnumerator FadeRoutine()
+    {
+        yield return new WaitForSeconds(Runner.RemoteRenderTime);
+
+        fadeController.FadeIn();
     }
 
 }
