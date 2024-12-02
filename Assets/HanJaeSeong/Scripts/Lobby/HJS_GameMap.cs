967d9f55ac2657b4fbf81e9541c21fd54f7cfb0d
scv829 <scv829@naver.com>
2024-12-02 15:45:58 +0900

    Feat: 게임 맵을 담고 있는 스크립트 추가
    게임 맵을 담고 있는 스크립트 추가
    
    Related to: #79

---
+++ Assets/HanJaeSeong/Scripts/Lobby/HJS_GameMap.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+/// <summary>
+/// 게임 맵을 담고 있는 스크립트
+/// </summary>
+public class HJS_GameMap : MonoBehaviour
+{
+    public static HJS_GameMap instance;
+
+    [Header("GameMap")]
+    [SerializeField] string[] gameScenes;
+
+    [Header("SelectMap")]
+    [SerializeField] List<string> selectSceneList;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public string NextMap()
+    {
+        if(selectSceneList.Count == 0)
+        {
+            return null;
+        }
+        int index = Random.Range(0, selectSceneList.Count);
+        string name = selectSceneList[index];
+        selectSceneList.RemoveAt(index);
+
+        return name;
+    }
+
+    public int SceneLength => gameScenes.Length;
+
+    public string GetName(int i) => gameScenes[i];
+
+    public void SetSelectMap(List<string> list)
+    {
+        ResetList();
+        foreach(string s in list)
+        {
+            selectSceneList.Add(s);
+        }
+    }
+
+    public bool SceneEmpty => selectSceneList.Count >= 1;
+
+    public bool ContainSelectScene(string name) => selectSceneList.Contains(name);
+
+    public void ResetList() => selectSceneList.Clear();
+
+}
