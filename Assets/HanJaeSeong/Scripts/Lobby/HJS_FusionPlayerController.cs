8c51d5c0cf2a75341fe6419ea36e4b23d1838ba1
scv829 <scv829@naver.com>
2024-12-02 15:48:27 +0900

    Feat: 플레이어 이동 고도화
    플레이어 이동 고도화

--- Assets/HanJaeSeong/Scripts/Lobby/HJS_FusionPlayerController.cs
+++ Assets/HanJaeSeong/Scripts/Lobby/HJS_FusionPlayerController.cs
@@ -4,6 +4,7 @@ using Firebase.Extensions;
 using Fusion;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -11,37 +12,66 @@ public class HJS_FusionPlayerController : NetworkBehaviour
 {
     private CharacterController _controller;
 
-    public float PlayerSpeed = 2f;
+    [SerializeField] TMP_Text nickname;
+    [SerializeField] float playerSpeed = 5f;
+    [SerializeField] float rotateSpeed = 90f;
+    private Vector3 moveDir;
+
+    // 네트워크 상에서의 위치
+    [Networked] Vector3 NetworkedPosition { get; set; }
+    [Networked] Quaternion NetworkedRotation { get; set; }
 
     private void Awake()
     {
         _controller = GetComponent<CharacterController>();
     }
 
+    public override void Spawned()
+    {
+       nickname.text = HJS_FirebaseManager.Auth.CurrentUser.DisplayName;
+    }
+
     // 접속이 끊어질 때 <- 파괴가 된다 <- 그때 데이터에 저장
     private void OnDestroy()
     {
-        HJS_PlayerPosition.Instance.PlayerPos = transform.position;
-    }
-
-    public override void FixedUpdateNetwork()
-    {
-        // Only move own player and not every other player. Each player controls its own player object.
         if (HasStateAuthority == false)
         {
             return;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Runner.DeltaTime * PlayerSpeed;
+        HJS_PlayerPosition.Instance.PlayerPos = transform.position;
+    }
 
-        if (move == Vector3.zero) return;
-
-        _controller.Move(move);
-
-        if (move != Vector3.zero)
+    public override void FixedUpdateNetwork()
+    {
+        if (HasStateAuthority == false)
         {
-            gameObject.transform.forward = move;
+            return;
         }
+
+        Vector3 move = Vector3.forward * moveDir.z * Runner.DeltaTime * playerSpeed;
+        float rotate = moveDir.x * rotateSpeed * Runner.DeltaTime;
+
+        // 모든 클라이언트에서 작동 x
+        if (Runner.IsSharedModeMasterClient)
+        {
+            // Master Client에서만 네트워크 상태를 업데이트
+            NetworkedPosition += transform.TransformDirection(move);
+            NetworkedRotation = Quaternion.Euler(0, transform.eulerAngles.y + rotate, 0);
+        }
+
+        // 위치를 동기화
+        transform.position = NetworkedPosition;
+        transform.rotation = NetworkedRotation;
+    }
+
+    private void Update()
+    {
+        // 소유권이 아니면 안하기
+        if (!HasStateAuthority) return;
+
+        moveDir.x = Input.GetAxis("Horizontal");
+        moveDir.z = Input.GetAxis("Vertical");
     }
 
     public void LeaveRoom()
